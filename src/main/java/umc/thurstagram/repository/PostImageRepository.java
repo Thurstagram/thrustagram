b01af6730d9fcf3463e0f07442df6c35078ae479
jeongyun <s6570490@naver.com>
2024-04-11 18:34:13 +0900

    domain

--- src/main/java/umc/thurstagram/repository/PostImageRepository.java
+++ src/main/java/umc/thurstagram/repository/PostImageRepository.java
@@ -1,4 +1,7 @@
 package umc.thurstagram.repository;
 
-public interface PostImageRepository extends Pja{
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.thurstagram.domain.PostImage;
+
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {
 }
